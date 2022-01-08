fe6c404980192aefaadaca916ce5294cec26997e
Mohammad Shanewaz Al Maruf <einfoengine@gmail.com>
2022-01-09 00:03:44 +0600

    First commit - Initial setup

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import { Fragment } from 'react';
+// import 'sass';
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css'
+import './Style/test.css'
+
+const App = () => {
+  return (
+    <Fragment>
+      <div className="container">
+      <h1>Hi I am diong my test!</h1>
+      </div>
+    </Fragment>
+  );
+}
+export default App;
